946d0e4b449855d7cee9b68cd485a9f242a69343
Anil Patidar <patidaranil0791@gmail.com>
2023-03-06 15:04:50 +0530

    RoomGPT

---
+++ components/Footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+  return (
+    <footer className="text-center h-16 sm:h-12 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3 border-white-500">
+    </footer>
+  );
+}
